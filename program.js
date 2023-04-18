6d1490f023a80381c81685a4ef4b2d749f7e2941
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-04-18 05:33:27 +0200

    complete book addition function

--- program.js
+++ program.js
@@ -4,13 +4,43 @@ function Book(title,author,nPages,read) {
     this.title=title;
     this.author=author;
     this.nPages=nPages;
-    this.read=read
-    this.info = () => `${this.title} by ${this.author}, ${this.nPages} pages, ${read}`
-
+    this.read=read;
 }
 
-function addBookToLibrary() {
-    // do stuff here
+function addBookToLibrary(book) {
+    const container=document.querySelector(".container")
+    const card=document.createElement("div");
+
+    const title=document.createElement("h3");
+    title.textContent=book.title
+
+    const author=document.createElement("p");
+    author.textContent=book.author
+
+    const nPages=document.createElement("p");
+    nPages.textContent=book.nPages
+
+    const status=document.createElement("div");
+    status.addEventListener("click",()=>{
+        status.classList.toggle("read")
+    });
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(nPages);
+    card.appendChild(status);
+
+    container.appendChild(card);
 }
 
-let book= new Book("The Hobbit","J.R.R. Tolkien",295,"not read yet")
+
+
+
+function displayBooks(library) {
+    library.array.forEach(book => {
+
+    });
+}
+
+
+addBookToLibrary(new Book("The Hobbit","J.R.R. Tolkien",295,true))
